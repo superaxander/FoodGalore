37ff4472c0db4719f1e3383ff6a1be9c3e5c26ac
anoniem.axander@gmail.com <anoniem.axander@gmail.com>
2015-01-24 18:23:10 +0100

    Initial Setup

---
+++ src/main/java/net/alexanders/FoodGalore/FoodGalore.java
@@ -0,0 +1,26 @@
+package net.alexanders.FoodGalore;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "FoodGalore", name = "Food Galore", version = "1.7.10-1.0")
+public class FoodGalore
+{
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent evt)
+    {
+        
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent evt)
+    {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent evt)
+    {
+
+    }
+}
